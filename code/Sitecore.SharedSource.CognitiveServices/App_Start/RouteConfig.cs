c107a1e3efb94eb2f1ba5cae9620734c4b69d5d2
Mark Stiles <stiles.mark@gmail.com>
2017-01-02 11:40:08 -0500

    *adding unicorn to serialize content *adding an editor tab using mvc routes to load the page

---
+++ code/Sitecore.SharedSource.CognitiveServices/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+
+namespace Sitecore.SharedSource.CognitiveServices.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+
+        {
+            routes.MapRoute(
+                name: "CognitiveServices", 
+                url: "sccogsvcs/{action}", 
+                defaults: new { controller = "SitecoreCognitiveServices" }
+            );
+        }
+    }
+
+    public class LoadRoutes
+    {
+        public void Process(PipelineArgs args)
+        {
+            Log.Info("Sitecore is loading Cognitive Services Routes", this);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ code/Sitecore.SharedSource.CognitiveServices/Controllers/SitecoreCognitiveServicesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Controllers;
+
+namespace Sitecore.SharedSource.CognitiveServices.Controllers
+{
+    public class SitecoreCognitiveServicesController : Controller
+    {
+        public SitecoreCognitiveServicesController()
+        {
+            Log.Info("hey", this);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
