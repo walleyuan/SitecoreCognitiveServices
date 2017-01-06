2f8a0efa90b2579b1add3f41d210be4061b07df1
Mark Stiles <stiles.mark@gmail.com>
2017-01-06 08:59:28 -0500

    *starting on adding a new ribbon

---
+++ code/Sitecore.SharedSource.CognitiveServices/Commands/ViewAnalysis.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Text;
+using System;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Sitecore.SharedSource.CognitiveServices.Commands
+{
+    [Serializable]
+    public class ViewAnalysis : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            UrlString urlString = new UrlString();
+            urlString.Append("en", context.Items[0].ID.ToString());
+            Sitecore.Shell.Framework.Windows.RunApplication("Security/Security Editor", urlString.ToString());
+        }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            Error.AssertObject((object)context, "context");
+            if (Context.Database.Items["/sitecore/content/Applications/Security/SecurityEditor"] == null)
+                return CommandState.Hidden;
+            return base.QueryState(context);
+        }
+    }
+}
