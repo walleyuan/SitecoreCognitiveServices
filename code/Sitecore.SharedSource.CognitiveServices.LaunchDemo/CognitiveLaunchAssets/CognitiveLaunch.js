c116f246967634af636dd8f798c66d0bba2383ee
Mark Stiles <stiles.mark@gmail.com>
2017-02-20 23:33:44 -0500

    *building out the giphy image request handler for the content moderator

--- code/Sitecore.SharedSource.CognitiveServices.LaunchDemo/CognitiveLaunchAssets/CognitiveLaunch.js
+++ code/Sitecore.SharedSource.CognitiveServices.LaunchDemo/CognitiveLaunchAssets/CognitiveLaunch.js
@@ -1,5 +1,38 @@
 ﻿jQuery.noConflict();
 jQuery(document).ready(function () {
+    
+    jQuery(".form-submit-image").click(function(e) {
+        e.preventDefault();
 
+        jQuery(".progress-indicator").show();
+        jQuery(".image-result").hide();
+        jQuery(".text-result").hide();
 
+        jQuery.getJSON("//api.giphy.com/v1/gifs/random?tag=" + jQuery(".moderator-query").val() + "&api_key=dc6zaTOxFJmzC")
+        .success(data => {
+            if (data.data.length < 1)
+                return;
+
+            var giphyUrl = data.data.image_url;
+
+            jQuery.post(
+                jQuery(".moderator-search-form").attr("action"),
+                {
+                    url: giphyUrl
+                }
+            ).done(function (r) {
+                jQuery(".image-result .image-file").html("<img src=\"" + giphyUrl + "\" />");
+                jQuery(".image-details .adult span").text(r.IsAdultContent + " (" + Math.round(100 * r.AdultScore) + "%)");
+                jQuery(".image-details .racy span").text(r.IsRacyContent + " (" + Math.round(100 * r.RacyScore) + "%)");
+                jQuery(".image-result").show();
+            });
+        });
+
+        jQuery(".progress-indicator").hide();
+    });
+
+    jQuery(".form-submit-text").click(function (e) {
+        e.preventDefault();
+
+    });
 });
--- code/Sitecore.SharedSource.CognitiveServices.LaunchDemo/Controllers/CognitiveLaunchController.cs
+++ code/Sitecore.SharedSource.CognitiveServices.LaunchDemo/Controllers/CognitiveLaunchController.cs
@@ -1,24 +1,32 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Sitecore.SharedSource.CognitiveServices.Services.Vision;
 
 namespace Sitecore.SharedSource.CognitiveServices.LaunchDemo.Controllers
 {
     public class CognitiveLaunchController : Controller
     {
-        //protected readonly ISomething Something;
+        protected readonly IVisionService VisionService;
         
         public CognitiveLaunchController(
-            //ISomething something
-            )
+            IVisionService visionService)
         {
-            //Assert.IsNotNull(something, typeof(ISomething));
-            
-            //Something = something;
+            VisionService = visionService;
         }
 
         public ActionResult Moderator()
         {
             return View();
+        }
+
+        public ActionResult ModerateImage(string url)
+        {
+            AnalysisResult ar = VisionService.AnalyzeImage(url, new List<VisualFeature>() {VisualFeature.Adult});
+
+            return Json(ar.Adult);
         }
     }
 }
