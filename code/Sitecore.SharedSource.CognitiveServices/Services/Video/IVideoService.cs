ecf2c5acb0176c41e26aaa215a61b95f29979cba
Mark Stiles <stiles.mark@gmail.com>
2017-02-28 10:43:26 -0500

    *adding Video service

--- code/Sitecore.SharedSource.CognitiveServices/Services/Video/IVideoService.cs
+++ code/Sitecore.SharedSource.CognitiveServices/Services/Video/IVideoService.cs
@@ -1,7 +1,14 @@
-﻿
+﻿using System.IO;
+using Microsoft.ProjectOxford.Video.Contract;
+
 namespace Sitecore.SharedSource.CognitiveServices.Services.Video
 {
     public interface IVideoService
     {
+        Operation CreateOperation(Stream video, VideoOperationSettings operationSettings);
+        Operation CreateOperation(byte[] video, VideoOperationSettings operationSettings);
+        Operation CreateOperation(string videoUrl, VideoOperationSettings operationSettings);
+        OperationResult GetOperationResult(Operation operation);
+        Stream GetResultVideo(string url);
     }
 }
--- code/Sitecore.SharedSource.CognitiveServices/Services/Video/VideoService.cs
+++ code/Sitecore.SharedSource.CognitiveServices/Services/Video/VideoService.cs
@@ -1,7 +1,103 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Video.Contract;
+using Sitecore.SharedSource.CognitiveServices.Foundation;
+using Sitecore.SharedSource.CognitiveServices.Repositories.Video;
+
 namespace Sitecore.SharedSource.CognitiveServices.Services.Video
 {
     public class VideoService : IVideoService
     {
+        protected IVideoRepository VideoRepository;
+        protected ILogWrapper Logger;
+
+        public VideoService(
+            IVideoRepository videoRepository,
+            ILogWrapper logger)
+        {
+            VideoRepository = videoRepository;
+            Logger = logger;
+        }
+        
+        public virtual Operation CreateOperation(Stream video, VideoOperationSettings operationSettings)
+        {
+            try
+            {
+                var result = Task.Run(async () => await VideoRepository.CreateOperationAsync(video, operationSettings)).Result;
+                
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("VideoService.CreateOperation failed", this, ex);
+            }
+
+            return null;
+        }
+
+        public virtual Operation CreateOperation(byte[] video, VideoOperationSettings operationSettings)
+        {
+            try
+            {
+                var result = Task.Run(async () => await VideoRepository.CreateOperationAsync(video, operationSettings)).Result;
+                
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("VideoService.CreateOperation failed", this, ex);
+            }
+
+            return null;
+        }
+
+        public virtual Operation CreateOperation(string videoUrl, VideoOperationSettings operationSettings)
+        {
+            try
+            {
+                var result = Task.Run(async () => await VideoRepository.CreateOperationAsync(videoUrl, operationSettings)).Result;
+                
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("VideoService.CreateOperation failed", this, ex);
+            }
+
+            return null;
+        }
+
+        public virtual OperationResult GetOperationResult(Operation operation)
+        {
+            try
+            {
+                var result = Task.Run(async () => await VideoRepository.GetOperationResultAsync(operation)).Result;
+                
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("VideoService.GetOperationResult failed", this, ex);
+            }
+
+            return null;
+        }
+
+        public virtual Stream GetResultVideo(string url)
+        {
+            try
+            {
+                var result = Task.Run(async () => await VideoRepository.GetResultVideoAsync(url)).Result;
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("VideoService.GetResultVideo failed", this, ex);
+            }
+
+            return null;
+        }
     }
 }
