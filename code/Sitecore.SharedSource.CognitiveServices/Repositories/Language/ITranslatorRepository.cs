388e2bf46916b46270a9c342017334ec03036bc8
Mark Stiles <stiles.mark@gmail.com>
2017-02-28 15:40:44 -0500

    *moved the translator to the language folder

--- code/Sitecore.SharedSource.CognitiveServices/Repositories/Language/ITranslatorRepository.cs
+++ code/Sitecore.SharedSource.CognitiveServices/Repositories/Language/ITranslatorRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Sitecore.SharedSource.CognitiveServices.Repositories.Speech {
+namespace Sitecore.SharedSource.CognitiveServices.Repositories.Language {
     public interface ITranslatorRepository {
         //https://github.com/MicrosoftTranslator/Csharp-Cmd-Line-Speech-Translate
     }
