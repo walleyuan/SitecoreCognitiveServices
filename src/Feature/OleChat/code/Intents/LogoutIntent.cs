5035d179488bccb19a10443d5267820ab86f0c86
Mark Stiles <stiles.mark@gmail.com>
2017-09-18 08:30:12 -0400

    * starting to add two new intents: logout and rebuild index

---
+++ src/Feature/OleChat/code/Intents/LogoutIntent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Sitecore.SharedSource.CognitiveServices.Wrappers;
+using Microsoft.SharedSource.CognitiveServices.Models.Language.Luis;
+using Sitecore.SharedSource.CognitiveServices.OleChat.Dialog;
+using Sitecore.SharedSource.CognitiveServices.OleChat.Models;
+
+namespace Sitecore.SharedSource.CognitiveServices.OleChat.Intents {
+
+    public interface ILogoutIntent : IIntent { }
+
+    public class LogoutIntent : BaseIntent, ILogoutIntent
+    {
+        protected readonly ITextTranslatorWrapper Translator;
+        
+        public override string Name => "logout";
+
+        public override string Description => "Logout the current user from Sitecore";
+        
+        public LogoutIntent(
+            ITextTranslatorWrapper translator,
+            IOleSettings settings) : base(settings) {
+            Translator = translator;
+        }
+
+        public override ConversationResponse ProcessResponse(LuisResult result, ItemContextParameters parameters, IConversation conversation) {
+
+            Sitecore.Security.Authentication.AuthenticationManager.Logout();
+            
+            return CreateConversationResponse($"You have been logged out.");
+        }
+    }
+}
---
+++ src/Feature/OleChat/code/Intents/RebuildIndexIntent.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SharedSource.CognitiveServices.Models.Language.Luis;
+using Sitecore.SharedSource.CognitiveServices.Wrappers;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.SharedSource.CognitiveServices.OleChat.Dialog;
+using Sitecore.SharedSource.CognitiveServices.OleChat.Models;
+using Sitecore.ContentSearch;
+using Sitecore.Jobs;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.ContentSearch.Maintenance;
+
+namespace Sitecore.SharedSource.CognitiveServices.OleChat.Intents {
+
+    public interface IRebuildIndexIntent : IIntent { }
+
+    public class RebuildIndexIntent : BaseIntent, IRebuildIndexIntent
+    {
+        protected readonly ITextTranslatorWrapper Translator;
+        protected readonly ISitecoreDataWrapper DataWrapper;
+        protected readonly IPublishWrapper PublishWrapper;
+        
+        public override string Name => "rebuild index";
+
+        public override string Description => "Rebuild an Index";
+
+        public override List<ConversationParameter> RequiredParameters => new List<ConversationParameter>()
+        {
+            new ConversationParameter(IndexKey, "What index do you want to rebuild?", IsIndexValid, IndexOptions)
+        };
+
+        #region Local Properties
+
+        protected string IndexKey = "Index Name";
+        
+        #endregion
+        
+        public RebuildIndexIntent(
+            ITextTranslatorWrapper translator,
+            IOleSettings settings) : base(settings)
+        {
+            Translator = translator;
+        }
+        
+        public override ConversationResponse ProcessResponse(LuisResult result, ItemContextParameters parameters, IConversation conversation)
+        {
+            //var toDb = (Database) conversation.Data[DBKey];
+            //PublishWrapper.PublishItem(rootItem, new[] { toDb }, new[] { rootItem.Language }, recursion, false);
+
+
+            ////////////////////////
+            //var index = (ISearchIndex)conversation.Data[IndexKey];
+            
+            //Job job2 = IndexCustodian.FullRebuild(index, true);
+            //while (job2 != null && !job2.IsDone)
+            //{
+            //    string str = string.Format("{0}: {1}. ", (object)this.Translate.TextByLanguage("Status", language), (object)this.Translate.TextByLanguage(job2.Status.State.ToString(), language));
+            //    if (job2.Status.State == JobState.Running)
+            //        str += string.Format("{0}: {1}.", (object)this.Translate.TextByLanguage("Processed", language), (object)job2.Status.Processed);
+            //    job1.Status.Messages.Add(str);
+            //    Thread.Sleep(500);
+            //}
+            //if (job2 == null || !job2.Status.Failed)
+            //    return;
+
+            ////////////////////////
+
+            /// rebuild all ///
+            /// 
+
+        //    Log.Audit("Rebuild all indexes", (object)this);
+        //    List<Job> list = IndexCustodian.RebuildAll(new IndexGroup[1]
+        //    {
+        //IndexGroup.Experience
+        //    }).ToList<Job>();
+        //    while (true)
+        //    {
+        //        List<Job> source = list;
+        //        Func<Job, bool> func = (Func<Job, bool>)(j => !j.IsDone);
+        //        Func<Job, bool> predicate;
+        //        if (source.Any<Job>(predicate))
+        //            Thread.Sleep(500);
+        //        else
+        //            break;
+        //    }
+        //    int num = list.Count<Job>((Func<Job, bool>)(j => j.Status.Failed));
+        //    if (num <= 0)
+        //        return;
+
+
+            //////////////////
+
+            return CreateConversationResponse($"message");
+        }
+
+        public virtual bool IsIndexValid(string paramValue, ItemContextParameters parameters, IConversation conversation)
+        {
+            var index = (conversation.Data.ContainsKey(IndexKey))
+                ? (ISearchIndex)conversation.Data[IndexKey]
+                : null;
+
+            if (index != null)
+                return true;
+
+            if (string.IsNullOrEmpty(paramValue))
+                return false;
+
+            index = ContentSearchManager.GetIndex(paramValue);
+            
+            if (index == null)
+                return false;
+
+            conversation.Data[IndexKey] = index;
+            return true;
+        }
+
+        public virtual Dictionary<string, string> IndexOptions()
+        {
+            return DataWrapper.GetDatabases().ToDictionary(a => a.Name, a => a.Name);
+        }
+    }
+}
