f19f04373c93070621022fad762279e50902f359
Mark Stiles <stiles.mark@gmail.com>
2017-12-16 13:25:29 -0500

    * starting an auto content tagging feature built on watson apis

---
+++ src/Feature/ContentTagging/code/App_Start/IocConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using SitecoreCognitiveServices.Feature.ContentTagging.Controllers;
+
+namespace SitecoreCognitiveServices.Feature.OleChat.App_Start
+{
+    public class IocConfig : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            //controller
+            serviceCollection.AddTransient(typeof(CognitiveContentTaggingController));
+        }
+    }
+}
---
+++ src/Feature/ContentTagging/code/Controllers/CognitiveContentTaggingController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Sitecore.Data.Managers;
+using SitecoreCognitiveServices.Feature.ContentTagging.Models;
+using SitecoreCognitiveServices.Foundation.SCSDK.Wrappers;
+
+namespace SitecoreCognitiveServices.Feature.ContentTagging.Controllers {
+
+    public class CognitiveContentTaggingController : Controller
+    {
+        protected readonly IWebUtilWrapper WebUtil;
+
+        protected readonly CreateClassifierModel Parameters;
+
+
+        public CognitiveContentTaggingController(
+            IWebUtilWrapper webUtil)
+        {
+            WebUtil = webUtil;
+            
+            Parameters = new CreateClassifierModel()
+            {
+                Id = WebUtil.GetQueryString("id"),
+                Language = WebUtil.GetQueryString("language"),
+                Database = WebUtil.GetQueryString("db")
+            };
+
+            ThemeManager.GetImage("Office/32x32/man_8.png", 32, 32);
+        }
+
+        public ActionResult CreateClassifier()
+        {
+            return View("CreateClassifier", Parameters);
+        }
+
+        public ActionResult Post()
+        {
+            return null;
+        }
+    }
+}
