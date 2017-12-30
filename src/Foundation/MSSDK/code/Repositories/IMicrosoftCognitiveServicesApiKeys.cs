a387fac29a94eee1cc19ddd6b10ecc63c9d92120
Mark Stiles <stiles.mark@gmail.com>
2017-12-30 13:52:24 -0500

    * wiring up api keys to the class instance

--- src/Foundation/MSSDK/code/Repositories/IMicrosoftCognitiveServicesApiKeys.cs
+++ src/Foundation/MSSDK/code/Repositories/IMicrosoftCognitiveServicesApiKeys.cs
@@ -5,41 +5,59 @@ namespace SitecoreCognitiveServices.Foundation.MSSDK
     {
         string Academic { get; }
         string AcademicEndpoint { get; }
+        string AcademicRetryInSeconds { get; }
         string BingSpeech { get; }
         string BingSpeechEndpoint { get; }
+        string BingSpeechRetryInSeconds { get; }
         string BingSpellCheck { get; }
         string BingSpellCheckEndpoint { get; }
+        string BingSpellCheckRetryInSeconds { get; }
         string BingAutosuggest { get; }
         string BingAutosuggestEndpoint { get; }
+        string BingAutosuggestRetryInSeconds { get; }
         string BingSearch { get; }
         string BingSearchEndpoint { get; }
+        string BingSearchRetryInSeconds { get; }
         string ComputerVision { get; }
         string ComputerVisionEndpoint { get; }
+        string ComputerVisionRetryInSeconds { get; }
         string ContentModerator { get; }
         string ContentModeratorClientId { get; }
         string ContentModeratorPrivateKey { get; }
         string ContentModeratorEndpoint { get; }
+        string ContentModeratorRetryInSeconds { get; }
         string EntityLinking { get; }
         string EntityLinkingEndpoint { get; }
+        string EntityLinkingRetryInSeconds { get; }
         string Emotion { get; }
         string EmotionEndpoint { get; }
+        string EmotionRetryInSeconds { get; }
         string Face { get; }
         string FaceEndpoint { get; }
+        string FaceRetryInSeconds { get; }
         string LinguisticAnalysis { get; }
         string LinguisticAnalysisEndpoint { get; }
+        string LinguisticAnalysisRetryInSeconds { get; }
         string Luis { get; }
         string LuisEndpoint { get; }
+        string LuisRetryInSeconds { get; }
         string QnA { get; }
         string QnAEndpoint { get; }
+        string QnARetryInSeconds { get; }
         string Recommendations { get; }
         string RecommendationsEndpoint { get; }
+        string RecommendationsRetryInSeconds { get; }
         string SpeakerRecognition { get; }
         string SpeakerRecognitionEndpoint { get; }
+        string SpeakerRecognitionRetryInSeconds { get; }
         string TextAnalytics { get; }
         string TextAnalyticsEndpoint { get; }
+        string TextAnalyticsRetryInSeconds { get; }
         string Video { get; }
         string VideoEndpoint { get; }
+        string VideoRetryInSeconds { get; }
         string WebLM { get; }
         string WebLMEndpoint { get; }
+        string WebLMRetryInSeconds { get; }
     }
 }
