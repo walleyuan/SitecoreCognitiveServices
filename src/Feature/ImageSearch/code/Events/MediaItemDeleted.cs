4f45b81ef2d46acbc0bdeb3e0743d2fb90a68b9f
Mark Stiles <stiles.mark@gmail.com>
2018-01-27 12:38:44 -0500

    * updating deleted event to check for the correct name and not check non media items

--- src/Feature/ImageSearch/code/Events/MediaItemDeleted.cs
+++ src/Feature/ImageSearch/code/Events/MediaItemDeleted.cs
@@ -6,15 +6,20 @@ using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Events;
 using SitecoreCognitiveServices.Feature.ImageSearch.Search;
+using SitecoreCognitiveServices.Foundation.SCSDK.Wrappers;
 
 namespace SitecoreCognitiveServices.Feature.ImageSearch.Events
 {
     public class MediaItemDeleted
     {
+        protected readonly ISitecoreDataWrapper DataWrapper;
         protected readonly IImageSearchService SearchService;
 
-        public MediaItemDeleted(IImageSearchService searchService)
+        public MediaItemDeleted(
+            ISitecoreDataWrapper dataWrapper,
+            IImageSearchService searchService)
         {
+            DataWrapper = dataWrapper;
             SearchService = searchService;
         }
 
@@ -23,7 +28,10 @@ namespace SitecoreCognitiveServices.Feature.ImageSearch.Events
             var item = Event.ExtractParameter(args, 0) as Item;
             Assert.IsNotNull(item, "No item in parameters");
 
-            var analysis = SearchService.GetImageAnalysisItem(item.ID.ToString(), item.Language.Name, item.Database.Name);
+            if (!DataWrapper.IsMediaFile(item))
+                return;
+
+            var analysis = SearchService.GetImageAnalysisItem(item.ID.ToShortID().ToString(), item.Language.Name, item.Database.Name);
 
             analysis?.Delete();
         }
