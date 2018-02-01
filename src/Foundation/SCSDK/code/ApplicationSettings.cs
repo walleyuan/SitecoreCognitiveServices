a0a83d5cf13046d151cc924a2a658a429d57988e
Mark Stiles <stiles.mark@gmail.com>
2018-02-01 13:36:04 -0500

    * moving settings to config

--- src/Foundation/SCSDK/code/ApplicationSettings.cs
+++ src/Foundation/SCSDK/code/ApplicationSettings.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Sitecore.Configuration;
 using Sitecore.Data;
 
 namespace SitecoreCognitiveServices.Foundation.SCSDK {
     public class ApplicationSettings : IApplicationSettings
     {
-        public virtual ID MSSDKId => new ID("{03BAF8AE-03CB-4E30-875C-0901EBA6A163}");
-        public virtual ID IBMSDKId => new ID("{503A5B1D-6F2F-41EC-9A77-AE1DAA37A987}");
+        public virtual ID MSSDKId => new ID(Settings.GetSetting("CognitiveService.MSSDK.ID"));
+        public virtual ID IBMSDKId => new ID(Settings.GetSetting("CognitiveService.IBMSDK.ID"));
         public virtual bool CatchAndReleaseExceptions
         {
             get
             {
-                var value = Sitecore.Configuration.Settings.GetSetting("CognitiveService.CatchAndReleaseExceptions");
+                var value = Settings.GetSetting("CognitiveService.CatchAndReleaseExceptions");
                 bool boolValue;
 
                 return (bool.TryParse(value, out boolValue)) && boolValue;
