c2416b1cdaf9fa60b3bdc1d78f4840289f42c3f2
Mark Stiles <stiles.mark@gmail.com>
2018-02-10 13:43:18 -0500

    * adding dictionary translation * surfacing new settings for the dictionary

--- src/Feature/OleChat/code/OleSettings.cs
+++ src/Feature/OleChat/code/OleSettings.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using Sitecore.Configuration;
 
 namespace SitecoreCognitiveServices.Feature.OleChat {
     public class OleSettings : IOleSettings
     {
-        public virtual Guid OleApplicationId => new Guid(Sitecore.Configuration.Settings.GetSetting("CognitiveService.OleApplicationId"));
+        public virtual Guid OleApplicationId => new Guid(Settings.GetSetting("CognitiveService.OleChat.OleApplicationId"));
+        public virtual string MasterDatabase => Settings.GetSetting("CognitiveService.OleChat.MasterDatabase");
+        public virtual string DictionaryDomain => Settings.GetSetting("CognitiveService.OleChat.DictionaryDomain");
     }
 }
---
+++ src/Feature/OleChat/code/Statics/Translator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data;
+using Sitecore.Globalization;
+
+namespace SitecoreCognitiveServices.Feature.OleChat.Statics
+{
+    public static class Translator
+    {
+        public static string Text(string key)
+        {
+            var settings = new OleSettings();
+            var db = Sitecore.Configuration.Factory.GetDatabase(settings.MasterDatabase);
+
+            using (new DatabaseSwitcher(db))
+            {
+                return Translate.TextByDomain(settings.DictionaryDomain, key) ?? string.Empty;
+            }
+        }
+    }
+}
