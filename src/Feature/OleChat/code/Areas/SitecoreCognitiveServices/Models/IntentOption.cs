917078cdf86a1a76022ed6bbff971971673d53c3
Mark Stiles <stiles.mark@gmail.com>
2018-02-14 09:21:13 -0500

    * removed unused model

--- src/Feature/OleChat/code/Areas/SitecoreCognitiveServices/Models/IntentOption.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SitecoreCognitiveServices.Feature.OleChat.Areas.SitecoreCognitiveServices.Models
-{
-    public class IntentOption
-    {
-        public string DisplayText { get; set; }
-        public string Value { get; set; }
-    }
-}
